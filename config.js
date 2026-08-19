1d317daceead7f8ecdf79fe99dcdc30a1a1b89ad
Sang <sang.ndt225664@sis.hust.edu.vn>
2026-08-19 23:30:45 +0700

    Insert blow-drying hair photo as photo5 and update config to 9 photos

--- config.js
+++ config.js
@@ -46,24 +46,30 @@ const CONFIG = {
         },
         {
             url: "images/photo5.jpg",
-            caption: "Những Khoảnh Khắc Bên Nhau 🍕✨",
+            caption: "Những Khoảnh Khắc Bên Nhau 💇‍♂️✨",
             date: "",
             desc: ""
         },
         {
             url: "images/photo6.jpg",
-            caption: "Những Khoảnh Khắc Bên Nhau ☕✨",
+            caption: "Những Khoảnh Khắc Bên Nhau 🍕✨",
             date: "",
             desc: ""
         },
         {
             url: "images/photo7.jpg",
-            caption: "Ảnh Photobooth Đầu Tiên Chụp Cùng Nhau 📸✨",
+            caption: "Những Khoảnh Khắc Bên Nhau ☕✨",
             date: "",
             desc: ""
         },
         {
             url: "images/photo8.jpg",
+            caption: "Ảnh Photobooth Đầu Tiên Chụp Cùng Nhau 📸✨",
+            date: "",
+            desc: ""
+        },
+        {
+            url: "images/photo9.jpg",
             caption: "Chúng Mình Đẹp Đôi Ghê 🥰✨",
             date: "",
             desc: ""
